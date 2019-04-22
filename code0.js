c06aa020c063e0002dfb74b064c676e1d9749dc3
madagascar1234 <49827952+madagascar1234@users.noreply.github.com>
2019-04-22 05:49:31 -0300

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,18 @@
+gdjs.NewSceneCode = {};
+
+gdjs.NewSceneCode.conditionTrue_0 = {val:false};
+gdjs.NewSceneCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.NewSceneCode.eventsList0xb25a8 = function(runtimeScene) {
+
+}; //End of gdjs.NewSceneCode.eventsList0xb25a8
+
+
+gdjs.NewSceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.NewSceneCode.eventsList0xb25a8(runtimeScene);
+return;
+}
+gdjs['NewSceneCode'] = gdjs.NewSceneCode;
